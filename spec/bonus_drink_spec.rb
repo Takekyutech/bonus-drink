3b997a57c60655626b48653b7a686ac58f86f6b4
Takekyutech <58509166+Takekyutech@users.noreply.github.com>
2021-02-24 02:42:44 +0800

    Update bonus_drink_spec.rb

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
+  
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
   specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
   specify { expect(BonusDrink.total_count_for(2)).to eq 2 }
