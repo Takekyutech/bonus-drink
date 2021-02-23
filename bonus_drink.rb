ea8d7f23859ee9436fdf395d053dc5849649348b
Takekyutech <58509166+Takekyutech@users.noreply.github.com>
2021-02-24 02:43:49 +0800

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,9 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    amount + amount.pred.abs / 2
+    def self.total_count_for(amount)
+      return 0 if amount.zero?
+  
+      last_amount = (amount - 1) % 2 + 1
+      (3 * amount - last_amount) / 2
+    end
   end
-end
+
